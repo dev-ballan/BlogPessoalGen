a85343a436fdf846c5596c48d0314b62a4a8c426
Allan Baad <munisallan@gmail.com>
2022-06-01 21:51:10 -0300

    Update Classe Tema do CRUD Back-end blog-pessoal Atv. 3

--- src/main/java/com/generation/blgpessoal/model/Postagem.java
+++ src/main/java/com/generation/blgpessoal/model/Postagem.java
@@ -6,12 +6,15 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
 import org.hibernate.annotations.UpdateTimestamp;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 @Entity
 @Table( name = "tb_postagens") // create table tb_postagens(
@@ -32,7 +35,19 @@ public class Postagem {
 	@UpdateTimestamp //@createTimestamp 
 	private LocalDateTime data;
 	
+	@ManyToOne
+	@JsonIgnoreProperties("postagem")
+	private Tema tema; // OBJETO TEMA PARA CRIAR A RELAÇÃO FK
 	
+	
+	public Tema getTema() {
+		return tema;
+	}
+
+	public void setTema(Tema tema) {
+		this.tema = tema;
+	}
+
 	public Long getId() {
 		return id;
 	}
